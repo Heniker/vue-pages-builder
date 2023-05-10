9b9953d6582cd3c9428195d89bd3b32c84a62667
Heniker <heniker@mail.ru>
2023-05-10 19:17:36 +0300

    rm cache & revert String conversion

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-declare type ContextT = ReturnType<typeof require.context>;
-export declare const buildPages: (weakContext: ContextT, persistentContext: ContextT, { prependPath, }?: {
-    prependPath?: string;
-}) => import("vue-router").RouteRecordRaw[];
-export {};
--- test/1/pages/parent/child2.vue
+++
@@ -1,7 +0,0 @@
-<template>{{ filePath }}</template>
-
-<script lang="ts">
-export default {
-  data() { return { filePath: __filename.slice(0, __filename.length - __resourceQuery.length) } }
-}
-</script>
